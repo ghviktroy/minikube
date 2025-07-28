7162c02e38d37e0fcfe97d71864a8c64ed4f03de
ghviktroy <94941872+ghviktroy@users.noreply.github.com>
2025-07-28 15:16:58 +0800

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+<!-- остальная HTML-разметка -->
